2dfd34374002b9eb7a142db499e48a518cabfcf7
Artem <Artem@alalalHome>
2021-04-25 19:44:12 +0300

    First commit

---
+++ src/main/java/knu/fit/demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package knu.fit.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Lab1Application.class);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : Apr 25, 2021, 7:33:52 PM
+    Author     : Artem
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
